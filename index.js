d0cac68deceb1710357910c34ce912068192a7c2
Nat Epstein <nathaniel.epstein@gmail.com>
2022-06-29 18:37:33 -0400

    added declare winner function to code

--- index.js
+++ index.js
@@ -33,3 +33,16 @@ if (randomNumber2 === 1) {
 } else {
     rightDice.setAttribute('src', "assets/images/dice6.png")
 }; 
+
+
+function declareWinner() {
+    if (randomNumber1 > randomNumber2) {
+        document.querySelector('h1').innerHTML = "Player 1 wins!";
+    } else if (randomNumber1 < randomNumber2) {
+        document.querySelector('h1').innerHTML = "Player 2 wins!";
+    } else {
+        document.querySelector('h1').innerHTML = "It's a tie!";
+    }
+}
+
+declareWinner();

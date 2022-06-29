e2a819940037f067482235fd00fd3cbb819ce388
Nat Epstein <nathaniel.epstein@gmail.com>
2022-06-29 18:35:50 -0400

    added queryselectors for dice 1 and 2

--- index.js
+++ index.js
@@ -2,3 +2,5 @@ let randomNumber1 = Math.floor(Math.random() * (6)) + 1;
 // console.log(randomNumber1);
 let randomNumber2 = Math.floor(Math.random() * (6)) + 1;
 
+let leftDice = document.querySelector(".img1");
+let rightDice = document.querySelector(".img2");
